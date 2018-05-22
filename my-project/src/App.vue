2ea11f316509ebf2467a3c3dae3ab600850be1d1
taohuaer <990324108@qq.com>
2018-05-22 15:57:20 +0800

    未完成品

---
+++ my-project/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <!--左侧导航-->
+    
+        <leftNav></leftNav>
+        
+    
+    <!--操作区域-->
+    <div class="main">
+    <router-view></router-view>
+    <Pos></Pos>
+    </div>
+  </div>
+</template>
+ 
+<script>
+import leftNav from '@/components/common/leftNav'
+import Pos from '@/components/page/Pos'
+export default {
+  name: 'app',
+  components:{
+    leftNav,
+    Pos
+  }
+}
+</script>
+ 
+<style>
+#app {
+  font-family: 'Microsoft YaHei','Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+   height:100%;
+}
+ 
+.main{
+  float:left;
+  width:95%; 
+  background-color: #EFF2F7;
+  height:100%;
+  overflow: auto;
+ 
+}
+*{
+  margin: 0;
+  padding:0;
+}
+li{
+  list-style: none;
+}
+</style>
